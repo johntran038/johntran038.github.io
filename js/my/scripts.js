99fa27be2fe8c46f4670d70148517f8c46ad97d1
inotp <tran6854@stthomas.edu>
2023-09-28 00:04:51 -0500

    responsive update

---
+++ js/my/scripts.js
@@ -0,0 +1,17 @@
+let back = document.getElementById('back');
+let middle = document.getElementById('middle');
+let front = document.getElementById('front');
+let welcomeText = document.getElementById('welcome-text');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    if(value>this.window.innerHeight){
+        value = this.window.innerHeight;
+        console.log("Aaa");
+    }
+
+    back.style.top = value * 0.8 + 'px';
+    middle.style.top = value * 0.5  + 'px';
+    front.style.top = value * 0  + 'px';
+    welcomeText.style.marginRight = value * 3  + 'px';
+});
