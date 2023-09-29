2cf075339ca6e922d23766ad77014841b6d8d6f0
inotp <tran6854@stthomas.edu>
2023-09-28 20:21:58 -0500

    added fun

--- js/my/scripts.js
+++ js/my/scripts.js
@@ -1,16 +1,14 @@
 let back = document.getElementById('back');
 let middle = document.getElementById('middle');
 let front = document.getElementById('front');
-let welcomeText = document.getElementById('welcome-text');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    // if(value>this.window.innerHeight){
-    //     value = this.window.innerHeight;
-    // }
+    if(value>this.window.innerHeight){
+        value = this.window.innerHeight;
+    }
 
     back.style.top = value * 0.8 + 'px';
     middle.style.top = value * 0.5  + 'px';
     front.style.top = value * 0  + 'px';
-    welcomeText.style.marginRight = value * 3  + 'px';
 });
