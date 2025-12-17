6821a8e452a3b49f333fd6fd453c5f627bf1a1d6
John Tran <123798873+johntran038@users.noreply.github.com>
2025-12-17 09:15:16 -0600

    in progress

---
+++ src/components/ProjectComponents/ProjectData.jsx
@@ -0,0 +1,18 @@
+
+import React, { useState } from "react";
+
+const ProjectData = ({  }) => {
+
+    
+
+    const projects = () => { 
+        
+        return [
+
+        ]
+    };
+
+    return {projects};
+};
+
+export default ProjectData;
